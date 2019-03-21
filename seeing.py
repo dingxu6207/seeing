0b52b8ce17f5077d16f539dd113b18a8b06a1070
dingxu6207 <32772701+dingxu6207@users.noreply.github.com>
2019-03-21 22:39:09 +0800

    添加python代码

---
+++ seeing.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 21 09:42:18 2019
+
+@author: dingxu
+"""
+
+
+import serial
+import time
+
+i = 0
+
+if __name__ == '__main__':
+    
+    f = open('E:/stm32_text/seeing/test.txt','w')
+    f.write('it is ok1')
+    serial = serial.Serial('COM5', 115200)  #/dev/ttyUSB0
+    
+   
+    if serial.isOpen() :
+        print("open success")
+    else :
+        print("open failed")       
+    
+        
+       
+    while True:
+      data = serial.readline()
+      if data != b'' :
+        print(data.decode("gbk")) 
+        f.write(data.decode("gbk"))        
+          
+      dt = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+      i = i + 1
+      if i == 5:
+        print (dt)
+        f.write(dt)
+        f.write('\n')  
+        i = 0
+        print(type(dt))
+        
+      t = (dt.find(' 22:25'))   
+      print(t)
+      if (t > 0):
+          f.close()
+          serial.close()
+    
+          
+
+   
+          
+ 
